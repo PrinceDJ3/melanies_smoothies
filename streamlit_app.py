95baeb26c16aa8ab93b55fedf575c0f2a9f159b2
David Sarkodie Junior <146789973+PrinceDJ3@users.noreply.github.com>
2026-06-24 14:47:12 +0100

    Display smoothie data as a dataframe
    Replace text display of smoothie data with a dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,9 @@ if ingredients_list:
 
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     
     # if ingredients_string:
     #     session.sql(my_insert_stmt).collect()
